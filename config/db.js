6c8d08e742d0eaa2af3ca4118ecaa1aa3558ad68
root <root@localhost.localdomain>
2021-06-23 17:30:47 +0100

    Setup the connection to database

--- config/db.js
+++ config/db.js
@@ -3,11 +3,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = mysql.createConnection({
+export default db = mysql.createConnection({
   host: process.env.MYSQL_ADDON_HOST,
   user: process.env.MYSQL_ADDON_USER,
   password: process.env.MYSQL_ADDON_PASSWORD,
   database: process.env.MYSQL_ADDON_DB,
 });
-
-export default db;
