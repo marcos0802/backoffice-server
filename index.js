84c6a3590ac5fa6141b1a5dd32579c86b555c6fa
root <root@localhost.localdomain>
2021-06-23 17:43:55 +0100

    fix : default route

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.redirect("/projects");
+});
+
 app.use("/projects", projectsRoutes);
 
 const PORT = process.env.PORT || 5000;
